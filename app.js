3b63dcc03599fcc1a323f37105a836c2b9aee2bb
Vamsi Aila <avamsi.web@gmail.com>
2020-01-04 14:56:42 +0530

    initial

---
+++ app.js
@@ -0,0 +1,26 @@
+// const a = (name) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if(name === 'priya') {
+//                 resolve(`${name} is a good girl`);
+//             } else {
+//                 reject(`${name} is a bad girl`);
+//             }
+//         }, 1500);
+//     });
+// }
+
+// // a('priya').then((msg) => {
+// //     console.log(msg);
+// // }).catch((e) => {
+// //     console.log(e);
+// // });
+// async function s() {
+//     const d= nodemon a('priya');
+//     console.log(d);
+//     return d;
+// }
+
+// s();
+// console.log('spjme');
+
